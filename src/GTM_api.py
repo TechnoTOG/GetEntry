260b7e8ec74e7fd37b383519dc84cfe96a3830d1
TechnoTOG <xaviererick879@gmail.com>
2023-08-27 20:14:17 +0530

    commit

---
+++ src/GTM_api.py
@@ -0,0 +1,26 @@
+#Entire code to be verified and accepted by @devkiraa and @TechnoTOG
+
+import os
+import csv
+import qrcode
+import random
+import string
+from tqdm import tqdm
+from PIL import Image
+from flask import Flask, request, jsonify
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+from email.mime.base import MIMEBase
+from email import encoders
+import requests
+
+#--------Code block for "QR generation" to be Generated,modified and updated by @GowriParvathyy--------
+
+
+#--------Code block for "Ticket generation" to be Generated,modified and updated by @niranjana_2004--------
+
+
+
+#--------Code block for "Mailing Service" to be Generated,modified and updated by @Devaah07--------
